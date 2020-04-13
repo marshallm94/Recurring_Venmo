4f97a1af9fb1a0317178991a83c65bf89ac458cc
Marshall McQuillen <marshallm94@gmail.com>
2020-04-12 20:10:40 -0500

    add pip3 installations

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ sudo apt-get -y install python3-pip
 sudo apt-get -y install vim 
 
 pip3 install venmo
+pip3 install httplib2
+pip3 install oauth2client
+pip3 install base64
+pip3 install email
+pip3 install apiclient
 
 # for Raspberry Pi setup - adds venmo directory to PATH
 echo 'export PATH=$PATH:/home/pi/.local/bin' >> ~/.bashrc
