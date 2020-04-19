c433a332275c5ec48e470c0b94b78e12579a2c7b
Marshall McQuillen <marshallm94@gmail.com>
2020-04-19 09:50:19 -0500

    Add logs for main & venmo separately

--- run_program.sh
+++ run_program.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # get bills to request money for and create bash script
-python3 main.py
+python3 main.py >> main_logs.log
 
 # execute venmo requests
-source venmo_requests_to_make.sh >> last_run_logs.log
+source venmo_requests_to_make.sh >> venmo_logs.log
 
 # clean up directory and prepare for next run
 rm venmo_requests_to_make.sh
