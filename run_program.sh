4bc9e7535e025ab6f0a92ab3e097c53ecaf4560e
Marshall McQuillen <marshallm94@gmail.com>
2020-05-24 08:36:20 -0500

    Make bash script executable for cron

--- run_program.sh
+++ run_program.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# >> = append to file if the file already has data
-# > _file_ = stdout will overwrite data in _file_
-# 2> _file_ = stderr will overwrite data in _file_ 
+# get bills to request money for and create bash script -
+# In english: "Run main.py and pipe and errors to main_errors.log and any output
+# to main_logs.log. If main.py succeeds, make venmo_requests_to_make.sh executable"
+python3 main.py 2> main_errors.log > main_logs.log && chmod +x venmo_requests_to_make.sh
 
-# get bills to request money for and create bash script
-python3 main.py > main_logs.log
-
-# execute venmo requests
-# pipe any errors to request_logs.log, stdout to venmo_logs.log and only remove
-# venmo_requests_to_make.sh if there weren't any any errors
-bash venmo_requests_to_make.sh 2> request_logs.log > venmo_logs.log && rm venmo_requests_to_make.sh
+# execute venmo requests -
+# In english: "Run venmo_requests_to_make.sh and pipe and errors to request_errors.log
+# and any output to venmo_logs.log. If venmo_requests_to_make.sh succeeds, delete the file."
+bash venmo_requests_to_make.sh 2> request_errors.log > venmo_logs.log && rm venmo_requests_to_make.sh
